f9d1f43375625325f79791b98dea357f7e2c895d
Alexey Veprev <aveprev27@gmail.com>
2017-03-01 23:58:30 +0300

    Align style with requirements.

--- Union-Find/Percolation/src/Percolation.java
+++ Union-Find/Percolation/src/Percolation.java
@@ -1,113 +1,113 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-	private int n;
-	private int numOfSites;
-	private int virtualTopSiteId;
-	private int virtualBottomSiteId;
-	private int[] field;
-	private WeightedQuickUnionUF uf;
+    private int n;
+    private int numOfSites;
+    private int virtualTopSiteId;
+    private int virtualBottomSiteId;
+    private int[] field;
+    private WeightedQuickUnionUF uf;
 
-	public Percolation(int n) throws IllegalArgumentException {
-		if (n <= 0) {
-			throw new IllegalArgumentException("Expected n > 0");
-		}
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Expected n > 0");
+        }
 
-		this.n = n;
-		this.numOfSites = n * n;
-		this.uf = new WeightedQuickUnionUF(this.numOfSites + 2);
-		this.virtualTopSiteId = this.numOfSites;
-		this.virtualBottomSiteId = this.numOfSites + 1;
+        this.n = n;
+        this.numOfSites = n * n;
+        this.uf = new WeightedQuickUnionUF(this.numOfSites + 2);
+        this.virtualTopSiteId = this.numOfSites;
+        this.virtualBottomSiteId = this.numOfSites + 1;
 
-		this.field = new int[this.numOfSites];
-		for (int i = 0; i < n * n; i++) {
-			this.field[i] = 0;
-		}
-	}
+        this.field = new int[this.numOfSites];
+        for (int i = 0; i < n * n; i++) {
+            this.field[i] = 0;
+        }
+    }
 
-	private int getId(int row, int col) {
-		return col - 1 + (row - 1) * this.n;
-	}
+    private int getId(int row, int col) {
+        return col - 1 + (row - 1) * this.n;
+    }
 
-	public void open(int row, int col) throws IndexOutOfBoundsException {
-		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
-			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
-		}
+    public void open(int row, int col) {
+        if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
+            throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
+        }
 
-		int id = this.getId(row, col);
-		this.field[id] = 1;
+        int id = this.getId(row, col);
+        this.field[id] = 1;
 
-		// Connect this site with opened neighbors.
-		if (id < this.n) {  // top row
-			uf.union(id, this.virtualTopSiteId);
+        // Connect this site with opened neighbors.
+        if (id < this.n) {  // top row
+            uf.union(id, this.virtualTopSiteId);
 
-			if (this.isOpen(row + 1, col)) {
-				uf.union(id, this.getId(row + 1, col));
-			}
-		}
-		else if (id >= this.n * (this.n - 1)) {  // bottom row
-			uf.union(id, this.virtualBottomSiteId);
+            if (this.isOpen(row + 1, col)) {
+                uf.union(id, this.getId(row + 1, col));
+            }
+        }
+        else if (id >= this.n * (this.n - 1)) {  // bottom row
+            uf.union(id, this.virtualBottomSiteId);
 
-			if (this.isOpen(row - 1, col)) {
-				uf.union(id, this.getId(row - 1, col));
-			}
-		}
-		else {  // middle rows
-			if (this.isOpen(row - 1, col)) {
-				uf.union(id, this.getId(row - 1, col));
-			}
+            if (this.isOpen(row - 1, col)) {
+                uf.union(id, this.getId(row - 1, col));
+            }
+        }
+        else {  // middle rows
+            if (this.isOpen(row - 1, col)) {
+                uf.union(id, this.getId(row - 1, col));
+            }
 
-			if (this.isOpen(row + 1, col)) {
-				uf.union(id, this.getId(row + 1, col));
-			}
-		}
+            if (this.isOpen(row + 1, col)) {
+                uf.union(id, this.getId(row + 1, col));
+            }
+        }
 
-		if (id == this.n * (row - 1)) { // left column
-			if (this.isOpen(row, col + 1)) {
-				uf.union(id, this.getId(row, col + 1));
-			}
-		}
-		else if (id == this.n * row - 1) { // right column
-			if (this.isOpen(row, col - 1)) {
-				uf.union(id, this.getId(row, col - 1));
-			}
-		}
-		else {  // middle columns
-			if (this.isOpen(row, col + 1)) {
-				uf.union(id, this.getId(row, col + 1));
-			}
+        if (id == this.n * (row - 1)) { // left column
+            if (this.isOpen(row, col + 1)) {
+                uf.union(id, this.getId(row, col + 1));
+            }
+        }
+        else if (id == this.n * row - 1) { // right column
+            if (this.isOpen(row, col - 1)) {
+                uf.union(id, this.getId(row, col - 1));
+            }
+        }
+        else {  // middle columns
+            if (this.isOpen(row, col + 1)) {
+                uf.union(id, this.getId(row, col + 1));
+            }
 
-			if (this.isOpen(row, col - 1)) {
-				uf.union(id, this.getId(row, col - 1));
-			}
-		}
-	}
+            if (this.isOpen(row, col - 1)) {
+                uf.union(id, this.getId(row, col - 1));
+            }
+        }
+    }
 
-	public boolean isOpen(int row, int col) throws IndexOutOfBoundsException {
-		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
-			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
-		}
-		return this.field[col - 1 + (row - 1) * this.n] == 1;
-	}
+    public boolean isOpen(int row, int col) {
+        if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
+            throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
+        }
+        return this.field[col - 1 + (row - 1) * this.n] == 1;
+    }
 
-	public boolean isFull(int row, int col) throws IndexOutOfBoundsException {
-		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
-			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
-		}
-		return this.field[col - 1 + (row - 1) * this.n] == 0;
-	}
+    public boolean isFull(int row, int col) {
+        if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
+            throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
+        }
+        return this.field[col - 1 + (row - 1) * this.n] == 0;
+    }
 
-	public int numberOfOpenSites() {
-		int sum = 0;
+    public int numberOfOpenSites() {
+        int sum = 0;
 
-		for (int i = 0; i < this.numOfSites; i++) {
-			sum += this.field[i];
-		}
+        for (int i = 0; i < this.numOfSites; i++) {
+            sum += this.field[i];
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 
-	public boolean percolates() {
-		return this.uf.connected(this.virtualTopSiteId, this.virtualBottomSiteId);
-	}
+    public boolean percolates() {
+        return this.uf.connected(this.virtualTopSiteId, this.virtualBottomSiteId);
+    }
 }
--- Union-Find/Percolation/src/PercolationStats.java
+++ Union-Find/Percolation/src/PercolationStats.java
@@ -2,78 +2,77 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-	private double mean;
-	private double stddev;
-	private double confidenceLo;
-	private double confidenceHi;
+    private double mean;
+    private double stddev;
+    private double confidenceLo;
+    private double confidenceHi;
 
-	private int doPercolationExperiment(int n) {
-		Percolation p = new Percolation(n);
-		int numOfBlockedSites = n * n;
-		int[] blockedSites = new int[numOfBlockedSites];
-		int ixOfSiteToOpen;
-		int siteToOpen;
-		int swap;
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("Expected: n > 0; trials > 0. Got: n = " + n + "; trials = " + trials);
+        }
 
-		for (int i = 0; i < numOfBlockedSites; i++) {
-			blockedSites[i] = i;
-		}
+        double[] results = new double[trials];
 
-		while (!p.percolates()) {
-			ixOfSiteToOpen = StdRandom.uniform(numOfBlockedSites);
-			siteToOpen = blockedSites[ixOfSiteToOpen];
+        for (int i = 0; i < trials; i++) {
+            results[i] = (double) this.doPercolationExperiment(n) / (double) (n * n);
+        }
 
-			p.open(siteToOpen / n + 1, siteToOpen % n + 1);
+        this.mean = StdStats.mean(results);
+        this.stddev = StdStats.stddev(results);
 
-			numOfBlockedSites--;
-			swap = blockedSites[numOfBlockedSites];
-			blockedSites[numOfBlockedSites] = siteToOpen;
-			blockedSites[ixOfSiteToOpen] = swap;
-		}
+        this.confidenceLo = this.mean - this.stddev * 1.96 / Math.sqrt(trials);
+        this.confidenceHi = this.mean + this.stddev * 1.96 / Math.sqrt(trials);
+    }
 
-		return p.numberOfOpenSites();
-	}
+    public double mean() {
+        return this.mean;
+    }
 
-	public PercolationStats(int n, int trials) throws IllegalArgumentException {
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalArgumentException("Expected: n > 0; trials > 0. Got: n = " + n + "; trials = " + trials);
-		}
+    public double stddev() {
+        return this.stddev;
+    }
 
-		double[] results = new double[trials];
+    public double confidenceLo() {
+        return this.confidenceLo;
+    }
 
-		for (int i = 0; i < trials; i++) {
-			results[i] = (double) this.doPercolationExperiment(n) / (double) (n * n);
-		}
+    public double confidenceHi() {
+        return this.confidenceHi;
+    }
 
-		this.mean = StdStats.mean(results);
-		this.stddev = StdStats.stddev(results);
+    private int doPercolationExperiment(int n) {
+        Percolation p = new Percolation(n);
+        int numOfBlockedSites = n * n;
+        int[] blockedSites = new int[numOfBlockedSites];
+        int ixOfSiteToOpen;
+        int siteToOpen;
+        int swap;
 
-		this.confidenceLo = this.mean - this.stddev * 1.96 / Math.sqrt(trials);
-		this.confidenceHi = this.mean + this.stddev * 1.96 / Math.sqrt(trials);
-	}
+        for (int i = 0; i < numOfBlockedSites; i++) {
+            blockedSites[i] = i;
+        }
 
-	public double mean() {
-		return this.mean;
-	}
+        while (!p.percolates()) {
+            ixOfSiteToOpen = StdRandom.uniform(numOfBlockedSites);
+            siteToOpen = blockedSites[ixOfSiteToOpen];
 
-	public double stddev() {
-		return this.stddev;
-	}
+            p.open(siteToOpen / n + 1, siteToOpen % n + 1);
 
-	public double confidenceLo() {
-		return this.confidenceLo;
-	}
+            numOfBlockedSites--;
+            swap = blockedSites[numOfBlockedSites];
+            blockedSites[numOfBlockedSites] = siteToOpen;
+            blockedSites[ixOfSiteToOpen] = swap;
+        }
 
-	public double confidenceHi() {
-		return this.confidenceHi;
-	}
+        return p.numberOfOpenSites();
+    }
 
-
-	public static void main(String[] args) {
-		PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-		System.out.println("mean                    = "  + ps.mean());
-		System.out.println("stddev                  = "  + ps.stddev());
-		System.out.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
-	}
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                    = "  + ps.mean());
+        System.out.println("stddev                  = "  + ps.stddev());
+        System.out.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 
 }
