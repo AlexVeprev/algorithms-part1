7889b3c18494b2c73c38d08ccda259cb2cde76f3
Alexey Veprev <aveprev27@gmail.com>
2017-03-01 23:24:04 +0300

    Percolation works.

--- Union-Find/Percolation/src/Percolation.java
+++ Union-Find/Percolation/src/Percolation.java
@@ -1,66 +1,113 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 	private int n;
-	private boolean[] field;
-	
+	private int numOfSites;
+	private int virtualTopSiteId;
+	private int virtualBottomSiteId;
+	private int[] field;
+	private WeightedQuickUnionUF uf;
+
 	public Percolation(int n) throws IllegalArgumentException {
 		if (n <= 0) {
 			throw new IllegalArgumentException("Expected n > 0");
 		}
 
 		this.n = n;
-		
-		this.field = new boolean[n * n];
+		this.numOfSites = n * n;
+		this.uf = new WeightedQuickUnionUF(this.numOfSites + 2);
+		this.virtualTopSiteId = this.numOfSites;
+		this.virtualBottomSiteId = this.numOfSites + 1;
+
+		this.field = new int[this.numOfSites];
 		for (int i = 0; i < n * n; i++) {
-			this.field[i] = false;
+			this.field[i] = 0;
 		}
+	}
+
+	private int getId(int row, int col) {
+		return col - 1 + (row - 1) * this.n;
 	}
 
 	public void open(int row, int col) throws IndexOutOfBoundsException {
 		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
 			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
 		}
-		
-		this.field[col - 1 + (row - 1) * this.n] = true;
+
+		int id = this.getId(row, col);
+		this.field[id] = 1;
+
+		// Connect this site with opened neighbors.
+		if (id < this.n) {  // top row
+			uf.union(id, this.virtualTopSiteId);
+
+			if (this.isOpen(row + 1, col)) {
+				uf.union(id, this.getId(row + 1, col));
+			}
+		}
+		else if (id >= this.n * (this.n - 1)) {  // bottom row
+			uf.union(id, this.virtualBottomSiteId);
+
+			if (this.isOpen(row - 1, col)) {
+				uf.union(id, this.getId(row - 1, col));
+			}
+		}
+		else {  // middle rows
+			if (this.isOpen(row - 1, col)) {
+				uf.union(id, this.getId(row - 1, col));
+			}
+
+			if (this.isOpen(row + 1, col)) {
+				uf.union(id, this.getId(row + 1, col));
+			}
+		}
+
+		if (id == this.n * (row - 1)) { // left column
+			if (this.isOpen(row, col + 1)) {
+				uf.union(id, this.getId(row, col + 1));
+			}
+		}
+		else if (id == this.n * row - 1) { // right column
+			if (this.isOpen(row, col - 1)) {
+				uf.union(id, this.getId(row, col - 1));
+			}
+		}
+		else {  // middle columns
+			if (this.isOpen(row, col + 1)) {
+				uf.union(id, this.getId(row, col + 1));
+			}
+
+			if (this.isOpen(row, col - 1)) {
+				uf.union(id, this.getId(row, col - 1));
+			}
+		}
 	}
-	
+
 	public boolean isOpen(int row, int col) throws IndexOutOfBoundsException {
 		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
 			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
 		}
-		return this.field[col - 1 + (row - 1) * this.n];
+		return this.field[col - 1 + (row - 1) * this.n] == 1;
 	}
-	
+
 	public boolean isFull(int row, int col) throws IndexOutOfBoundsException {
 		if (row <= 0 || row > this.n || col <= 0 || col > this.n) {
 			throw new IndexOutOfBoundsException("Expected 0 < row <= " + this.n + " and  0 < col <= " + this.n);
 		}
-		return !this.field[col - 1 + (row - 1) * this.n];
+		return this.field[col - 1 + (row - 1) * this.n] == 0;
 	}
-	
+
 	public int numberOfOpenSites() {
-		return 0;
+		int sum = 0;
+
+		for (int i = 0; i < this.numOfSites; i++) {
+			sum += this.field[i];
+		}
+
+		return sum;
 	}
-	
+
 	public boolean percolates() {
-		return false;
+		return this.uf.connected(this.virtualTopSiteId, this.virtualBottomSiteId);
 	}
-	   
-	public static void main(String[] args) {
-		Percolation p = new Percolation(5);
-		p.open(5, 5);
-		p.open(1, 5);
-		p.open(5, 1);
-		p.open(1, 1);
-		p.open(1, 1);
-
-		System.out.println(p.isFull(1, 1));
-		System.out.println(p.isOpen(1, 1));
-		System.out.println(p.isFull(2, 2));
-		System.out.println(p.isOpen(2, 2));
-	}
-
 }
